47364f0d45d0d3e67ba4f1332d12127198bbdb0d
=indefinitee <danny.galaev@gmail.com>
2024-04-16 12:36:19 +0400

    late-upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '22aad73e-e106-4631-9195-2a7f872c8627';
+export const MODAL_CLOSE_SEC = 2.5;
